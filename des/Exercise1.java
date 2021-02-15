a764e437311d790cb27facaf8e9cb8fa8aa3afe3
daisytraining01 <64245212+daisytraining01@users.noreply.github.com>
2021-02-15 21:08:56 +0530

    Subjective_5005

---
+++ des/Exercise1.java
@@ -0,0 +1,27 @@
+package step.des;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+//import org.junit.Test;
+
+public class Exercise1 {
+
+	public static void main(String[] args) { 
+		System.setProperty("webdriver.gecko.driver","  ");
+	    String baseURL = "http://demo.guru99.com/test/newtours/register.php";
+	    WebDriver driver = new FirefoxDriver();
+		driver.get(baseURL);
+
+		Select drpCountry = new Select(driver.findElement(By.name("country")));
+		drpCountry.selectByVisibleText("AUSTRALIA");
+
+		//Selecting Items in a Multiple SELECT elements
+		/*driver.get("http://jsbin.com/osebed/2");
+		Select fruits = new Select(driver.findElement(By.id("fruits")));
+		fruits.selectByVisibleText("Banana");
+		fruits.selectByIndex(1);*/
+ }
+
+}
---
+++ des/Exercise4b.java
@@ -0,0 +1,29 @@
+package step.des;
+
+import java.text.ParseException;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Exercise4b {
+
+	public static void main(String[] args) throws ParseException {
+    	WebDriver wd;
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\training\\eclipse-workspace\\Descriptive1\\src\\main\\resources\\chromedriver.exe");
+	  wd= new ChromeDriver();
+        wd.get("https://cosmocode.io/automation-practice-webtable/\r\n"
+        		+ "Belarus");         
+        //No.of Columns
+        List  col = wd.findElements(By.xpath(".//*[@id=\"leftcontainer\"]/table/thead/tr/th"));
+        System.out.println("No of cols are : " +col.size()); 
+        //No.of rows 
+        List  rows = wd.findElements(By.xpath(".//*[@id='leftcontainer']/table/tbody/tr/td[1]")); 
+        System.out.println("No of rows are : " + rows.size());
+       
+    }
+	
+	
+}
